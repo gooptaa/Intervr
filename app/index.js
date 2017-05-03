50fb349d5a555c29efa37d7833c66fd324910df5
mchen5517 <michael.chen.cse@gmail.com>
2017-05-03 16:24:08 -0400

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import store from './store.jsx';
+import store from './store';
 import '../public/stylesheets/index.scss'
 
 ReactDOM.render(
