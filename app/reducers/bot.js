e8e7fac6cf3273459fe7b79d3e356f2d9222dac9
Edward Goo <Edwardx89@yahoo.com>
2017-05-11 13:47:17 -0400

    added reducers for bot

---
+++ app/reducers/bot.js
@@ -0,0 +1,42 @@
+/*****
+This will keep track of the bot's interview questions.
+*****/
+import axios from 'axios';
+
+/* -------------------<   ACTIONS   >--------------------- */
+const GET_QUESTIONS = "GET_QUESTIONS";
+
+/* ---------------<   ACTION CREATORS   >------------------- */
+const getQuestions = (questions) => ({questions, type: GET_QUESTIONS});
+
+/* -------------------<   REDUCERS   >--------------------- */
+initialState = {
+  general: [],
+  technical: [],
+  intro: [],
+}
+
+export default function botReducer (state = initialState, action) {
+  switch(action.type){
+    case GET_QUESTIONS:
+      return Object.assign({}, state, action);
+    default: return state;
+  }
+}
+
+/* ------------------<   DISPATCHERS   >-------------------- */
+export const getAllQuestions= () => dispatch => {
+  let general = axios.get('/general')
+  .then((res) => res.data)
+  .catch(next)
+  let intro = axios.get('/intro')
+  .then((res) => res.data)
+  .catch(next)
+  let technical = axios.get('/technical')
+  .then((res) => res.data)
+  .catch(next)
+
+  Promise.all([general, intro, technical])
+  .then((resolved) => )
+}
+
