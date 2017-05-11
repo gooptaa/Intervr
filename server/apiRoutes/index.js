0c82d36d1a2057fdb8ba39d8684af4f435109210
Christopher <gupta.christopher@gmail.com>
2017-05-11 10:58:48 -0400

    Update index.js

--- server/apiRoutes/index.js
+++ server/apiRoutes/index.js
@@ -12,7 +12,7 @@ router.use(function (req, res, next) {
 //Get random general question from our database
 router.get('/general', (req, res, next) => {
   console.log('in the general route')
-  General.findById(Math.floor(Math.random() * (10)))
+  General.findById(Math.ceil(Math.random() * (10)))
   .then((question) => {
     res.json(question);
   })
@@ -21,7 +21,7 @@ router.get('/general', (req, res, next) => {
 //Get random technical question from our database
 router.get('/technical', (req, res, next) => {
   console.log('in the technical route')
-  Technical.findById(Math.floor(Math.random() * (6)))
+  Technical.findById(Math.ceil(Math.random() * (6)))
   .then((question) => {
     res.json(question);
   })
@@ -30,7 +30,7 @@ router.get('/technical', (req, res, next) => {
 //Get random intro question from our database
 router.get('/intro', (req, res, next) => {
   console.log('in the intro route')
-  Intro.findById(Math.floor(Math.random() * (6)))
+  Intro.findById(Math.ceil(Math.random() * (6)))
   .then((question) => {
     res.json(question);
   })
