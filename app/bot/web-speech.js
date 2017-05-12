aef0879472982aace2c9c18e9c3e39ae22f5faa8
Edward Goo <Edwardx89@yahoo.com>
2017-05-12 12:16:28 -0400

    refactored web-speech to a class function

--- app/bot/web-speech.js
+++ app/bot/web-speech.js
@@ -1,22 +1,48 @@
 //This is using Web Speech API Speech Synthesis. It will convert the text to speech on the client's browser.
 
-export default function speak(text){
-  if (text !== ''){
-    const utterThis = new SpeechSynthesisUtterance(text);
-    const selectedOption = 'Alex';
+// export default function speak(text){
+//   if (text !== ''){
+//     const utterThis = new SpeechSynthesisUtterance(text);
+//     const selectedOption = 'Alex';
+//     for (let i = 0; i < speechSynthesis.getVoices().length ; i++) {
+//       if (speechSynthesis.getVoices()[i].name === selectedOption) {
+//         utterThis.voice = speechSynthesis.getVoices()[i];
+//       }
+//     }
+//     utterThis.pitch = 1;
+//     utterThis.rate = 1;
+//     speechSynthesis.speak(utterThis);
+//   }
+// }
+
+// //To get a list of voices:
+// export const getVoices = function() {
+//   const synth = window.speechSynthesis;
+//   console.log(synth.getVoices());
+// }
+
+
+export default class Speak {
+  constructor(){
+    this.utterThis = null;
+    this.selectedOption = 'Alex';
+  }
+
+  on(text){
+    this.utterThis = new SpeechSynthesisUtterance(text);
     for (let i = 0; i < speechSynthesis.getVoices().length ; i++) {
-      if (speechSynthesis.getVoices()[i].name === selectedOption) {
-        utterThis.voice = speechSynthesis.getVoices()[i];
+      if (speechSynthesis.getVoices()[i].name === this.selectedOption) {
+        this.utterThis.voice = speechSynthesis.getVoices()[i];
       }
     }
-    utterThis.pitch = 1;
-    utterThis.rate = 1;
-    speechSynthesis.speak(utterThis);
+    this.utterThis.pitch = 1;
+    this.utterThis.rate = 1;
+    speechSynthesis.speak(this.utterThis);
   }
-}
 
-//To get a list of voices:
-export const getVoices = function() {
-  const synth = window.speechSynthesis;
-  console.log(synth.getVoices());
+  pause(){
+    if(this.utterThis) {
+      this.utterThis.pause()
+    }
+  }
 }
