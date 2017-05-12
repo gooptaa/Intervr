f2091312fd9c371b2248fdc70032cae1f0a77aa5
Michael Chen <michael.chen.cse@gmail.com>
2017-05-12 12:44:23 -0400

    fixing self reducer

--- app/index.js
+++ app/index.js
@@ -3,17 +3,19 @@ import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
 import store from './store';
 import '../public/stylesheets/index.scss';
-import Room from './browser/room';
-import Home from './browser/home';
 import {Router, browserHistory, Route} from 'react-router';
 import {generateWebRTC} from './webrtc/client';
 import {randomToken} from './util';
 import {setWebRTC} from './reducers/webrtc';
+import Room from './browser/room';
+import Home from './browser/home';
 import BotRoom from './browser/bot-room';
+import Launch from './browser/launch';
 
 const RoutesComponent = ({onRoomEnter}) => (
   <Router history={browserHistory}>
-    <Route path="/" component={Home} />
+    <Route path="/" component={Launch} />
+    <Route path="/lobby" component={Home} />
     <Route path="/peer-room" component={Room} onEnter={onRoomEnter}/>
     <Route path="/bot-room" component={BotRoom} />
   </Router>
--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import peerReducer from './peer';
 import webRTCReducer from './webrtc';
 import cameraReducer from './camera';
+import selfReducer from './self';
 
 
 const rootReducer = combineReducers({
   peer: peerReducer,
   webRTC: webRTCReducer,
-  camera: cameraReducer
+  camera: cameraReducer,
+  self: selfReducer
 });
 
 export default rootReducer;
