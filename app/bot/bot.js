6afbf3630446e9d6cd5fe706d30409b31744886a
Christopher Gupta <gupta.christopher@gmail.com>
2017-05-12 12:16:00 -0400

    levels adjusted and speaker added

--- app/bot/bot.js
+++ app/bot/bot.js
@@ -1,7 +1,7 @@
 import Speak from './web-speech';
 
 export default class Bot {
-  constructor(username = '', soundLevel = 100, threshold = 15){
+  constructor(username = '', soundLevel = 100, threshold = 30){
     this.soundLevel = soundLevel
     this.threshold = threshold
     this.waitCount = 0
@@ -12,17 +12,19 @@ export default class Bot {
     this.username = username
     this.questions = {}
     this.intervalID = null
+    this.source = null
+    this.Speaker = new Speak()
   }
 
-  setup(questions, fftsize = 2048, smoother = 0.8){
+  setup(questions, fftsize = 4096, smoother = 0.65){
     this.questions = questions
     this.analyzer.fftsize = fftsize
     this.analyzer.smoothingTimeConstant = smoother
     window.navigator.getUserMedia({
       audio: true
     }, (stream) => {
-      const source = this.audioCtx.createMediaStreamSource(stream)
-      source.connect(this.analyzer)
+      this.source = this.audioCtx.createMediaStreamSource(stream)
+      this.source.connect(this.analyzer)
       // this.analyzer.connect(this.audioCtx.destination)
     }, (err) => {
       console.error("Hmm, there was an issue setting up your room: ", err)
@@ -30,7 +32,7 @@ export default class Bot {
     setTimeout(this.next(), 5000)
   }
 
-  poll(freq = 200){
+  poll(freq = 100){
     this.polling = true
     this.intervalID = setInterval( () => {
         let data = new Float32Array(this.analyzer.frequencyBinCount)
@@ -68,24 +70,24 @@ export default class Bot {
   next(){ //introduce, ask questions, end interview
     if (this.questionsAsked === 0){
       let question = this.getQuestion('intro')
-      Speak(`Hello ${this.username}! Let's begin the interview. ${question}.`)
+      this.Speaker.on(`Hello ${this.username}! Let's begin the interview. ${question}.`)
       this.questionsAsked++
       this.poll()
     }
     else if (this.questionsAsked < 2){
       let question = this.getQuestion('intro')
-      Speak(`Great! ${question}`)
+      this.Speaker.on(`Great! ${question}`)
       this.questionsAsked++
       this.poll()
     }
     else if (this.questionsAsked < 6){
       let question = this.getQuestion('general')
-      Speak(`Great! ${question}`)
+      this.Speaker.on(`Great! ${question}`)
       this.questionsAsked++
       this.poll()
     }
     else {
-      Speak('Great! That concludes the interview. Feel free to exit and reenter the app to practice some more.')
+      this.Speaker.on('Great! That concludes the interview. Feel free to exit and reenter the app to practice some more.')
     }
   }
 
@@ -101,6 +103,9 @@ export default class Bot {
     this.poll = null
     this.next = null
     this.getQuestion = null
+    this.source = null
+    this.audioCtx.close()
+    this.Speaker.pause()
   }
 }
 
