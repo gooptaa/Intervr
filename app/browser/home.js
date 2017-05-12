9341900446e2b5e3f8369788ffad05a9e4c1302d
Michael Chen <michael.chen.cse@gmail.com>
2017-05-12 11:25:35 -0400

    launch page skele

--- app/browser/home.js
+++ app/browser/home.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from 'react';
+import { Link } from 'react-router';
 import aframe from 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
---
+++ app/browser/launch.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+class LaunchComponent extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default connect(null, null)(LaunchComponent);
