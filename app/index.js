2f7e33c0d1dc749e9368d7f1edd314ee0032b75c
Michael Chen <michael.chen.cse@gmail.com>
2017-05-13 23:34:00 -0400

    fix rooms (cheesed it with hash)

--- app/index.js
+++ app/index.js
@@ -3,20 +3,22 @@ import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
 import store from './store';
 import '../public/stylesheets/index.scss';
-import {Router, browserHistory, Route} from 'react-router';
+import {Router, browserHistory, Route, Redirect} from 'react-router';
 import {generateWebRTC} from './webrtc/client';
 import {setWebRTC} from './reducers/webrtc';
+import {updateRoom} from './reducers/self';
 import Room from './browser/room';
 import Home from './browser/home';
 import BotRoom from './browser/bot-room';
 import Launch from './browser/launch';
-import { updateHandle } from './reducers/self';
 
-const RoutesComponent = ({onRoomEnter}) => (
+const RoutesComponent = ({onRoomEnter, onPeerRoomEnter}) => (
   <Router history={browserHistory}>
     <Route path="/" component={Launch} />
     <Route path="/lobby" component={Home} />
-    <Route path="/peer-room" component={Room} onEnter={onRoomEnter}/>
+    {/*<Redirect path="/peer-room" to={`/peer-room/${store.getState().self.room}`} />
+    <Route path="/peer-room/:roomName" component={Room} onEnter={onRoomEnter}/>*/}
+    <Route path="/peer-room" component={Room} onEnter={onPeerRoomEnter} />
     <Route path="/bot-room" component={BotRoom} />
   </Router>
 )
@@ -24,7 +26,16 @@ const RoutesComponent = ({onRoomEnter}) => (
 const mapProps = null;
 const mapDispatch = (dispatch, ownProps) => ({
   onRoomEnter: (nextRouterState) => {
-    dispatch(setWebRTC(generateWebRTC(store.getState().self.room)));
+    dispatch(setWebRTC(generateWebRTC(nextRouterState.params.roomName)));
+  },
+  onPeerRoomEnter: (nextRouterState) => {
+    var room = window.location.hash.substring(1);
+    if (!room) {
+      window.location.hash = store.getState().self.room;
+    } else {
+      dispatch(updateRoom(room));
+      dispatch(setWebRTC(generateWebRTC(room)));
+    }
   }
 });
 
